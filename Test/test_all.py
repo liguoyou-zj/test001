a489ab31c93d45bc278ac5ba28b3c85d1ebf70c4
经理 <manager@153.com>
2018-12-08 15:38:31 +0800

    '添加多一个测试函数'

--- Test/test_all.py
+++ Test/test_all.py
@@ -13,3 +13,11 @@ class Test_allure:
     def test_al(self, a):
         allure.attach('描述', '我是测试步骤001的描述～～～')
         assert a != 2
+
+
+    @pytest.mark.parametrize("a", [1, 2, 3])
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @allure.step('我是测试步骤001')
+    def test_a2(self, a):
+        allure.attach('描述', '我是测试步骤002的描述～～～')
+        assert a != 2
